41063c665028d93c9964f3168861ada8f7fa5600
Salvatore D'Agostino <toto19@gmail.com>
2018-06-26 23:49:29 -0400

    update model for DB support

--- models/event.go
+++ models/event.go
@@ -1,13 +1,17 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+
+	"github.com/go-sql-driver/mysql"
+)
 
 // Event is the definition of an event that we want to track
 type Event struct {
-	ID         string                 `json:"id,omitempty"`
-	Name       string                 `json:"name,omitempty"`
-	Properties map[string]interface{} `json:"properties,omitempty"`
-	CreatedOn  time.Time              `json:"created_on,omitempty"`
-	UpdatedOn  time.Time              `json:"updated_on,omitempty"`
-	DeletedOn  time.Time              `json:"deleted_on,omitempty"`
+	ID         string          `json:"id,omitempty"`
+	Name       string          `json:"name,omitempty"`
+	Properties json.RawMessage `json:"properties,omitempty"`
+	CreatedOn  mysql.NullTime  `db:"created_on" json:"created_on,omitempty"`
+	UpdatedOn  mysql.NullTime  `db:"updated_on" json:"updated_on,omitempty"`
+	DeletedOn  mysql.NullTime  `db:"deleted_on" json:"deleted_on,omitempty"`
 }
