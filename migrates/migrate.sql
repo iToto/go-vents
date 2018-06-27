0d886b28b180e214124f0a91813ca7de752cbebf
Salvatore D'Agostino <toto19@gmail.com>
2018-06-26 22:12:00 -0400

    add DB migrations and seeds

--- migrates/migrate.sql
+++ migrates/migrate.sql
@@ -3,12 +3,27 @@
 /**
  DROP TABLE IF EXISTS
 */
+DROP TABLE IF EXISTS events;
 
 /**
  CREATE TABLE
 */
 
+CREATE TABLE events
+(
+id UUID UNIQUE ,
+name VARCHAR(255) UNIQUE ,
+properties JSONB,
+"created_on" TIMESTAMPTZ,
+"updated_on" TIMESTAMPTZ,
+"deleted_on" TIMESTAMPTZ,
+CONSTRAINT "events_pkey" PRIMARY KEY (id)
+);
+
 /**
  CREATE INDEX
  ADD FOREIGN KEY
 */
+
+CREATE INDEX "events_id_idx" ON events(id);
+CREATE INDEX "events_name_idx" ON events(name);
--- migrates/seeds.sql
+++ migrates/seeds.sql
@@ -1,1 +1,8 @@
 -- SQL SEEDS
+INSERT INTO public.events (id, name, properties, created_on)
+    VALUES (
+        (SELECT uuid_generate_v4()),
+        'test_event',
+        '{"foo":"bar"}',
+        NOW()
+    );
