cabd435f2faf38dda343dc98e6d6f4840c5c15eb
Salvatore D'Agostino <toto19@gmail.com>
2018-06-28 09:37:54 -0400

    switch from mysql to pq

--- models/event.go
+++ models/event.go
@@ -3,7 +3,7 @@ package models
 import (
 	"encoding/json"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/lib/pq"
 )
 
 // SetEvent is the definition of an event that we want to track
@@ -11,7 +11,7 @@ type SetEvent struct {
 	ID         string          `json:"id,omitempty"`
 	Name       string          `json:"name,omitempty"`
 	Properties json.RawMessage `json:"properties,omitempty"`
-	CreatedOn  mysql.NullTime  `db:"created_on" json:"created_on,omitempty"`
-	UpdatedOn  mysql.NullTime  `db:"updated_on" json:"updated_on,omitempty"`
-	DeletedOn  mysql.NullTime  `db:"deleted_on" json:"deleted_on,omitempty"`
+	CreatedOn  pq.NullTime     `db:"created_on" json:"created_on,omitempty"`
+	UpdatedOn  pq.NullTime     `db:"updated_on" json:"updated_on,omitempty"`
+	DeletedOn  pq.NullTime     `db:"deleted_on" json:"deleted_on,omitempty"`
 }
